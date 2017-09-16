eaf26fe26fc52b7118533843ad8eecdd4c4e42cf
tgetmanova <tatyana.getmanova@gmail.com>
2017-09-17 01:44:08 +0300

    Get emails array from JSON

--- utils/github_service.js
+++ utils/github_service.js
@@ -6,6 +6,25 @@ module.exports = {
         var request = new XMLHttpRequest();
         request.open("GET", "https://api.github.com/user/emails", false, context.userLogin, context.userPassword);
         request.send();
-        return request;
+
+        var emailsJson = JSON.parse(request.responseText);
+
+        var emailAddresses = [];
+        for (var i = 0; i < emailsJson.length; i++) {
+            var item = emailsJson[i];
+            var emailAddress = this.initializeEmail(item["email"], item["primary"], item["verified"], item["visibility"]);
+            emailAddresses.push(emailAddress);
+        }
+
+        return emailAddresses;
+    },
+
+    initializeEmail: function (email, primary, verified, visibility) {
+        var emailAddress = {};
+        emailAddress.email = email;
+        emailAddress.primary = primary;
+        emailAddress.verified = verified;
+        emailAddress.visibility = visibility;
+        return emailAddress;
     }
 }
